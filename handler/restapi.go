1b5c22e5bf1cce80d78e36b270b7695097e1f770
firefly1126 <xiaoming1109@gmail.com>
2021-04-12 11:44:32 +0800

    enhance api

--- handler/restapi.go
+++ handler/restapi.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"crypto"
-	"crypto/rand"
+	"crypto/ed25519"
 	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
 	"errors"
 	"fennec/core"
 	"fennec/handler/param"
@@ -33,6 +36,7 @@ func createUserHandlerFunc(dapp *core.Wallet) http.HandlerFunc {
 		var params struct {
 			WalletName string `json:"wallet_name"`
 			CipherType string `json:"cipher_type"`
+			PrivateKey string `json:"private_key"`
 		}
 
 		if e := param.Binding(r, &params); e != nil {
@@ -45,6 +49,11 @@ func createUserHandlerFunc(dapp *core.Wallet) http.HandlerFunc {
 			return
 		}
 
+		if params.PrivateKey == "" {
+			render.BadRequest(w, errors.New("no private key"))
+			return
+		}
+
 		if params.CipherType == "" {
 			params.CipherType = "rsa"
 		}
@@ -52,9 +61,19 @@ func createUserHandlerFunc(dapp *core.Wallet) http.HandlerFunc {
 		var signer crypto.Signer
 
 		if params.CipherType == "ed25519" {
-			signer = mixin.GenerateEd25519Key()
+			prv, err := parseED25519PrivateKeyFromStr(params.PrivateKey)
+			if err != nil {
+				render.BadRequest(w, errors.New("parse ed25519 private key error"))
+				return
+			}
+			signer = prv
 		} else {
-			signer = generateRSAKey()
+			prv, err := parseRSAPrivateKeyFromPEMStr(params.PrivateKey)
+			if err != nil {
+				render.BadRequest(w, errors.New("parse rsa private key error"))
+				return
+			}
+			signer = prv
 		}
 
 		user, keyStore, err := dapp.Client.CreateUser(ctx, signer, params.WalletName)
@@ -76,7 +95,25 @@ func createUserHandlerFunc(dapp *core.Wallet) http.HandlerFunc {
 	}
 }
 
-func generateRSAKey() *rsa.PrivateKey {
-	private, _ := rsa.GenerateKey(rand.Reader, 1024)
-	return private
+func parseRSAPrivateKeyFromPEMStr(pemStr string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(pemStr))
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
+
+func parseED25519PrivateKeyFromStr(str string) (ed25519.PrivateKey, error) {
+	content, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
 }
